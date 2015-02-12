29769590b9fa47907d25f4fb1bdf7b35f23faaaf
Jacob M. Slone <jslone1993@gmail.com>
2015-02-12 13:58:39 -0500

    reworking ui

---
+++ Assets/Sudoku/Scripts/UIButton.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ButtonAction {
+	GAME_EASY,
+	GAME_MED,
+	GAME_HARD,
+	GAME_NEW,
+	MENU_MAIN,
+	MENU_GAME_NEW,
+	MENU_OPTIONS,
+	MENU_SCORES,
+	QUIT
+}
+
+public class UIButton : MonoBehaviour {
+	public ButtonAction action;
+	
+	public void OnClick() {
+		switch(action) {
+			case ButtonAction.GAME_EASY:
+				PlayerPrefs.SetString("gameLevel", "easy");
+				goto case ButtonAction.GAME_NEW;
+			case ButtonAction.GAME_MED:
+				PlayerPrefs.SetString("gameLevel", "medium");
+				goto case ButtonAction.GAME_NEW;
+			case ButtonAction.GAME_HARD:
+				PlayerPrefs.SetString("gameLevel", "hard");
+				goto case ButtonAction.GAME_NEW;
+			case ButtonAction.GAME_NEW:
+				Application.LoadLevel(4); // game scene
+				break;
+			
+			case ButtonAction.MENU_MAIN:
+				Application.LoadLevel(0);	// menu scene
+				break;
+			case ButtonAction.MENU_GAME_NEW:
+				Application.LoadLevel(1);	// new game scene
+				break;
+			case ButtonAction.MENU_OPTIONS:
+				Application.LoadLevel(2);	// options scene
+				break;
+			case ButtonAction.MENU_SCORES:
+				Application.LoadLevel(3);	// scores scene
+				break;
+			case ButtonAction.QUIT:
+				Application.Quit();
+				break;
+		}
+	}
+}
