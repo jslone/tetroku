c9ff5fb82652f4031e435a1223ea1613217445cd
Jacob M. Slone <jslone1993@gmail.com>
2015-02-12 19:15:45 -0500

    missing files

---
+++ Assets/Sudoku/Scripts/UISound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum SOUND_TYPE {
+	MUSIC,
+	SOUND
+};
+
+public class UISound : MonoBehaviour {
+	public SOUND_TYPE type;
+	private Sound sound;
+	// Use this for initialization
+	void Start () {
+		sound = GameObject.FindObjectOfType<Sound>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void OnSlide(float v) {
+		switch(type) {
+			case SOUND_TYPE.MUSIC:
+				PlayerPrefs.SetFloat("musicVolume",v);
+				sound.ChangeMusicVolume(v);
+				break;
+			case SOUND_TYPE.SOUND:
+				PlayerPrefs.SetFloat("soundVolume",v);
+				sound.ChangeSoundVolume(v);
+				break;
+		}
+	}
+}
---
+++ Assets/UIScore.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Difficulty {
+	EASY,
+	MEDIUM,
+	HARD
+};
+
+public class UIScore : MonoBehaviour {
+	public Difficulty difficulty;
+	
+	// Use this for initialization
+	void Start () {
+		int time;
+		switch(difficulty) {
+			case Difficulty.EASY:
+			case Difficulty.MEDIUM:
+			case Difficulty.HARD:
+		}
+		int time = PlayerPrefs.GetInt("")
+		GetComponent<UI.Text>().text = PlayerPrefs.GetInt()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
