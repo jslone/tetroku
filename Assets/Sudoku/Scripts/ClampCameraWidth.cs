9468cf9718611642414ef15c1906d3f65d6fb30b
Jacob M. Slone <jslone1993@gmail.com>
2015-02-15 19:58:48 -0500

    fixing ui sizing, camera sizing

---
+++ Assets/Sudoku/Scripts/ClampCameraWidth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClampCameraWidth : MonoBehaviour {
+	public float minWidth;
+	public float maxSize;
+	
+	// Use this for initialization
+	void Start () {
+		maxSize = camera.orthographicSize;
+		Resize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		#if UNITY_EDITOR
+		Resize();
+		#endif
+	}
+	
+	void Resize() {
+		float width = camera.orthographicSize * camera.aspect;
+		float adjSize = minWidth / camera.aspect;
+		
+		camera.orthographicSize = Mathf.Max (maxSize,adjSize);
+		/*if(width < minWidth) {
+			camera.orthographicSize = minWidth / camera.aspect;
+		}*/
+	}
+}
