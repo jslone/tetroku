75fd0961c93e90052208340bfacc5414f67ea634
Jacob M. Slone <jslone1993@gmail.com>
2015-02-19 20:53:10 -0500

    code to take screenshots

---
+++ Assets/Sudoku/Scripts/Screenshot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshot : MonoBehaviour {
+	static int count = 0;
+	static Screenshot _instance = null;
+	// Use this for initialization
+	void Awake () {
+		if(_instance == null) {
+			_instance = this;
+			DontDestroyOnLoad(gameObject);
+			#if UNITY_EDITOR
+			InvokeRepeating("Take",3.0f,3.0f);
+			#endif
+		} else {
+			Destroy(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void Take() {
+		Debug.Log("called");
+		Application.CaptureScreenshot("Screenshots/screenshopt" + count++ + ".png");
+	}
+}
